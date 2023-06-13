134e0b1d82ff88de3b13e33c8338d410d9c54d3d
Eyal Izhaki <eyal1izhaki@gmail.com>
2023-06-13 19:55:59 +0300

    Handled failure when trying to get url Added logs

--- htmlscraper/scraper.py
+++ htmlscraper/scraper.py
@@ -4,14 +4,13 @@ import requests
 import logging
 import os
 
-from .const import URL_REGEX
 from .utils import get_html_filename_from_url
 
 
 class Scraper:
 
     def __init__(self, url, max_depth, max_per_page, uniqueness, saved_data_path, url_extractors, ignore_ssl_verification=False) -> None:
-        
+
         self._root_url = url
         self._max_depth = max_depth
         self._max_per_page = max_per_page
@@ -20,12 +19,18 @@ class Scraper:
         self._url_extractors = url_extractors
         self.ignore_ssl_verification = ignore_ssl_verification
 
+        self._scraped_counter = 0
+        self._failed_counter = 0
+
+        self._scraped_urls = []
+
         self._threads = []
         self._result = []
 
     def _write_html_to_file(self, html, depth, url):
 
-        path = os.path.join(self._saved_data_path, str(depth), get_html_filename_from_url(url))
+        path = os.path.join(self._saved_data_path, str(
+            depth), get_html_filename_from_url(url))
         dirname = os.path.dirname(path)
 
         os.makedirs(dirname, exist_ok=True)
@@ -36,19 +41,35 @@ class Scraper:
 
     def _get_html(self, url: str) -> bytes:
 
-        response = requests.get(url, verify=self.ignore_ssl_verification)
+        try:
+            response = requests.get(url, verify=not self.ignore_ssl_verification)
+        except:
+            logging.info(f"Failed to scrape {url}.")
+            self._failed_counter += 1
+            return None
+
+
         return response.content
 
     def _scrape(self, url, depth):
 
-        logging.info(f"Scraping {url} at depth {depth}")
 
-        if depth == self._max_depth:
+        if depth > self._max_depth:
             return
+        
+        scrapes_per_url = self._max_per_page
+
+        self._scraped_counter += 1
+        logging.info(f"Scraping {url} at depth {depth}")
 
         # TODO: Arguments order not consistent
         html = self._get_html(url)
+
+        if html is None:
+            return 
+
         self._write_html_to_file(html, depth, url)
+
         html_str = str(html)
 
         self._result.append(
@@ -58,16 +79,25 @@ class Scraper:
             }
         )
 
-        sub_urls = self._extract_urls(html_str, self._max_per_page)
+        sub_urls = self._extract_urls(html_str)
 
         for sub_url in sub_urls:
+
+            if self._uniqueness == True and sub_url in self._scraped_urls:
+                continue
+
+            if scrapes_per_url == 0:
+                break
 
             thread = threading.Thread(
                 target=self._scrape, args=[sub_url, depth+1])
             self._threads.append(thread)
             thread.start()
 
-    def _extract_urls(self, html: str, first_n):
+            self._scraped_urls.append(sub_url)
+            scrapes_per_url-= 1
+
+    def _extract_urls(self, html: str, first_n=-1):
 
         urls = []
 
@@ -86,9 +116,10 @@ class Scraper:
 
         for thread in self._threads:
             thread.join()
-        
+
         self._threads = []
         result = self._result
         self._result = []
 
+        logging.info(f"Scraped {self._scraped_counter} websites. Failed to scrape {self._failed_counter} ")
         return result
