4ff7a2a7a2436517df00ccea62a7bb633405c98d
Eyal Izhaki <eyal1izhaki@gmail.com>
2023-06-13 16:49:25 +0300

    Implemented function that builds filename from url

---
+++ htmlscraper/utils.py
@@ -0,0 +1,23 @@
+from .const import ILLEGAL_FILENAME_CHARS
+
+def get_html_filename_from_url(url: str):
+
+    additional_chars_to_be_replaced = ['.']
+
+    stripped_url = url.removeprefix('https://').removeprefix('http://')
+
+    filename = ''
+    for char in stripped_url:
+
+        if char in ILLEGAL_FILENAME_CHARS:
+            filename += '_'
+            continue
+        
+        if char in additional_chars_to_be_replaced:
+            filename += '_'
+            continue
+            
+        filename += char
+    
+    return filename + '.html'
+    
