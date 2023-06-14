fead010c84ab679089a880912c5eefef3d497c33
Eyal Izhaki <eyal1izhaki@gmail.com>
2023-06-14 21:33:42 +0300

    Implemented async get request and async write to file functions

--- htmlscraper/utils.py
+++ htmlscraper/utils.py
@@ -1,3 +1,6 @@
+import asyncio
+import requests
+
 from .const import ILLEGAL_FILENAME_CHARS
 
 def get_html_filename_from_url(url: str):
@@ -20,4 +23,19 @@ def get_html_filename_from_url(url: str):
         filename += char
     
     return filename + '.html'
+    
+
+async def async_get(url, *args, **kwargs):
+    return await asyncio.to_thread(requests.get, url, *args, **kwargs)
+
+async def async_write_to_file(path: str, content: bytes):
+
+    def sync_write():
+        try:
+            with open(path, 'wb') as file:
+                file.write(content)
+        except:
+            pass
+
+    return await asyncio.to_thread(sync_write)
     
